fd82b70c6ffb28a891498bae95150fc8d4a400c7
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-10-20 15:23:39 +0200

    data types

--- app.ts
+++ app.ts
@@ -9,3 +9,28 @@ function add(num1: number, num2: number) {
 button?.addEventListener('click', function(){
     console.log(add(+input1.value, +input2.value));   
 }) 
+
+
+///type of data simpe
+
+let num: number;
+ 
+num = 10; //цілі
+num = -10; // відємні
+num = 10.3; //дробові
+
+
+let string: string;
+string = "Some";
+
+let bool: boolean;
+bool = true;
+
+///type of data complex
+
+
+let arr:string[] = [];
+arr = ['string'];
+
+let arrNumber:number[] = [];
+arrNumber = [1, 2, 3];
